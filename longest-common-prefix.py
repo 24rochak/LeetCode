94fda4e01644bfdd1158b2fe174bb4447778c03f
Rochak <24rochak@gmail.com>
2019-06-20 21:36:51 +0530

    RomanToInt

---
+++ longest-common-prefix.py
@@ -0,0 +1,24 @@
+def longestCommonPrefix(strs) -> str:
+    if len(strs) == 0:
+        return ""
+    elif len(strs) == 1:
+        return strs[0]
+    else:
+        lens = [len(item) for item in strs]
+        lenmax = min(lens)
+        if lenmax == 0:
+            return ""
+        it = 0
+        while it < lenmax:
+            chars = []
+            for item in strs:
+                chars.append(item[it])
+            if len(set(chars)) == 1:
+                it += 1
+            else:
+                break
+        return strs[0][:it]
+
+
+prefix = longestCommonPrefix(["c", "c"])
+print(prefix)
