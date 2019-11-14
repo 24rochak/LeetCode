fcfa16bc881b638fa2acabe9fca2163643f6ee21
Rochak <24rochak@gmail.com>
2019-11-14 01:37:58 -0500

    Reverse Polish Notation

---
+++ Word Search.py
@@ -0,0 +1,44 @@
+def dfs(board, i, j, word):
+    if len(word) == 0:
+        return True
+    if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or word[0] != board[i][j]:
+        return False
+    tmp = board[i][j]
+    board[i][j] = "#"
+    # check whether can find "word" along one direction
+    res = dfs(board, i + 1, j, word[1:]) or \
+          dfs(board, i - 1, j, word[1:]) or \
+          dfs(board, i, j + 1, word[1:]) or \
+          dfs(board, i, j - 1, word[1:])
+    board[i][j] = tmp
+    return res
+
+
+def exist(board, word):
+    db = {}
+    for r in board:
+        for c in r:
+            if c not in db:
+                db[c] = 1
+            else:
+                db[c] += 1
+
+    dw = {}
+    for c in word:
+        if c not in dw:
+            dw[c] = 1
+        else:
+            dw[c] += 1
+        if c not in db or dw[c] > db[c]:
+            return False
+
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if dfs(board, i, j, word):
+                return True
+    return False
+
+
+board = [["a", "a"]]
+word = "aaa"
+print(exist(board, word))
