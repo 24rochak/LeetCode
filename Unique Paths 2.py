5e8269e484e89cd091b0eb179bef98765da5fdfe
Rochak <24rochak@gmail.com>
2019-11-24 14:52:11 -0500

    Unique Paths 2

--- Unique Paths 2.py
+++ Unique Paths 2.py
@@ -6,31 +6,24 @@ def uniquePathsWithObstacles(matrix):
     if matrix[0][0] == 1 or matrix[-1][-1] == 1:
         return 0
 
-    matrix[0][0] = 1
-    for i in range(1, len(matrix)):
-        if not matrix[i][0]:
-            matrix[i][0] = matrix[i - 1][0]
-        else:
-            matrix[i][0] = 0
-
-    for i in range(1, len(matrix[0])):
-        if not matrix[0][i]:
-            matrix[0][i] = matrix[0][i - 1]
-        else:
-            matrix[0][i] = 0
-
-    for i in range(1, len(matrix)):
-        for j in range(1, len(matrix[0])):
-            matrix[i][j] = int(not matrix[i][j])
-
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
             if matrix[i][j] == 1:
-                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1]
-            else:
                 matrix[i][j] = 0
-    for row in matrix:
-        print(row)
+            elif i == 0 and j == 0:
+                matrix[i][j] = 1
+            elif i == 0 and j > 0:
+                matrix[i][j] = matrix[i][j - 1]
+            elif j == 0 and i > 0:
+                matrix[i][j] = matrix[i - 1][j]
+            else:
+                matrix[i][j] = matrix[i][j - 1] + matrix[i - 1][j]
     return matrix[-1][-1]
 
 
-grid = [[0, 1, 0]]
+grid = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0]
+]
 print(uniquePathsWithObstacles(grid))
